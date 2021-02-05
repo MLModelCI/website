a982ab33cdc77286d8347d90bb3f78808e81e3f6
huangyz0918 <huangyz0918@gmail.com>
2021-02-05 19:49:34 +0800

    💄 ui: add svg animation.

--- .umirc.ts
+++ .umirc.ts
@@ -9,6 +9,8 @@ export default defineConfig({
   base: '/website',
   publicPath: '/website/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  polyfill: false,
+  styles: ['./app.less'],
   navs: {
     'en-US': [
       null,
